215c2a0a345ad504e22e1694c532fe6588f650f2
Scott Kriel <scottkriel@gmail.com>
2021-06-28 12:54:06 +0200

    scan reading

--- testScript.m
+++ testScript.m
@@ -9,12 +9,13 @@ serverDir ='$HOME/SKAAP/SKAAPserver/';
 clientDir = pwd;
 
 ssh_conn = ssh2_config(HOSTNAME,USERNAME,PASSWORD);
-command=sprintf('cd %s; python3 temp_humidity.py',serverDir);
-ssh_conn = ssh2_command(ssh_conn, command);
-command=sprintf('cd %s; python3 detect_devices.py',serverDir);
-ssh_conn = ssh2_command(ssh_conn, command);
-command=sprintf('cd %s; python3 device_info.py',serverDir);
-ssh_conn = ssh2_command(ssh_conn, command);
+
+% command=sprintf('cd %s; python3 temp_humidity.py',serverDir);
+% ssh_conn = ssh2_command(ssh_conn, command);
+% command=sprintf('cd %s; python3 detect_devices.py',serverDir);
+% ssh_conn = ssh2_command(ssh_conn, command);
+% command=sprintf('cd %s; python3 device_info.py',serverDir);
+% ssh_conn = ssh2_command(ssh_conn, command);
 % command=sprintf('cd %s; python3 get_samples.py --bins 2048',serverDir);
 % ssh_conn = ssh2_command(ssh_conn, command);
 % scp_get(ssh_conn,'output.txt',clientDir,serverDir);
@@ -24,9 +25,9 @@ ssh_conn = ssh2_command(ssh_conn, command);
 % plot(imag(samples),'DisplayName', 'Quadrature')
 % title('Raw time samples')
 % 
-ssh_conn = ssh2_close(ssh_conn);
-% fc = 94e06; % Centre frequency
-% N=65536; % Number of samples/bins. Max N=65536
+% 
+% fc = 92e06; % Centre frequency
+% N=2048; % Number of samples/bins. Max N=65536
 % gain=15.0; % Total gain
 % sampleRate=10e06; % Sample rate: either 2.5MHz or 10MHz
 % device_id_str='0'; % SDR device ID: '0' or '1'
@@ -49,23 +50,40 @@ ssh_conn = ssh2_close(ssh_conn);
 % title(string(dtime_spec))
 % 
 % 
-% command=sprintf('cd %s; soapy_power -r %d -f %d -b %d -g %.2f -k 0 -n 1 -F rtl_power_fftw -O scan_output.txt --debug',serverDir,sampleRate,fc,N,gain);
+% command=sprintf('cd %s; soapy_power -r %d -f 70M:120M -b %d -g %.2f -k 10 -n 1 -F rtl_power_fftw -O scan_output.txt --debug',serverDir,sampleRate,N,gain);
 % ssh_conn = ssh2_command(ssh_conn, command);
-% scp_get(ssh_conn,{'scan_output.txt'},clientDir,serverDir);
-% scan_data=readmatrix(strcat(clientDir,'\scan_output.txt'));
-% freq_scan=scan_data(:,1);
-% mag_scan=scan_data(:,2);
+scp_get(ssh_conn,{'scan_output.txt'},clientDir,serverDir);
+scan_data=readmatrix('scan_output.txt','delimiter',' ','CommentStyle','#');
+freq_scan=scan_data(:,1);
+mag_scan=scan_data(:,2);
+freqN = numel(freq_scan);
+freqU = numel(unique(freq_scan));
+nRun=floor(freqN/freqU);
+percComp=mod(freqN/freqU,1)*1e2;
+disp(['We are ',num2str(percComp),'%', ' complete with run ',num2str(nRun)])
+
+
+freqMAT=reshape(freq_scan(1:freqU*nRun),freqU,nRun);
+magMAT=reshape(mag_scan(1:freqU*nRun),freqU,nRun).';
+figure 
+plot(freqMAT(:,1)./1e06,magMAT,'LineStyle','none','Marker','.','MarkerSize',1e-12)
+xlabel('Frequency (MHz)')
+ylabel('Magnitude (dB)')
+grid on 
+grid minor
 % 
-% figure
-% plot(freq_scan./1e06,mag_scan)
-% grid on
-% grid minor
-% xlabel('Frequency (MHz)')
-% ylabel('Magnitude (dB)')
-% grid on 
-% grid minor
-% 
-% 
+figure
+plot(freqMAT(:,1)./1e06,max(magMAT), 'color','r','DisplayName','Max'); hold on,
+plot(freqMAT(:,1)./1e06,mean(magMAT), 'color','g','DisplayName','Mean'); hold on,
+plot(freqMAT(:,1)./1e06,min(magMAT), 'color','b','DisplayName','Min'); hold on,
+grid on
+grid minor
+xlabel('Frequency (MHz)')
+ylabel('Magnitude (dB)')
+grid on 
+grid minor
+
+
 
 
 % ssh2_conn = scp_simple_get(HOSTNAME,USERNAME,PASSWORD,'data0.npy','$HOME/SKAAP/');
@@ -76,3 +94,4 @@ ssh_conn = ssh2_close(ssh_conn);
 % ssh2_conn.command_result
 % 
 % command_output = ssh2_simple_command(HOSTNAME,USERNAME,PASSWORD,'python $HOME/SKAAP/run_measurement.py')
+ssh_conn = ssh2_close(ssh_conn);
